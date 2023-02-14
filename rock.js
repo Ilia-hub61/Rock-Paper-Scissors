e9894d95616fb09d0bf08993043261d9f93a98bc
Ilia-hub61 <ilag61477@gmail.com>
2023-02-14 17:05:51 +0300

    release2

--- rock.js
+++ rock.js
@@ -1,40 +1,38 @@
 function getComputerChoice(nomber){
     return Math.floor(Math.random() * nomber);
 }
-if (getComputerChoice(3) == 0){
-    alert('Rock')} else if (getComputerChoice(3) == 1){
-    alert('Scissors')} else {
-    alert('Paper')};
-    
+
 function playRound(playerSelection, computerSelection) {
+    let playerSelection = prompt("Who's your fighter");
         if (playerSelection.toLowerCase() === 'rock'){
-            if (computerSelection.toLowerCase() === 'rock'){
+            if (computerSelection === 1){
                 return 'Draw. Rock does not beat rock'
-        } else if (computerSelection.toLowerCase() === 'scissors'){
+        } else if (computerSelection === 2){
             return 'Win. Rock beats scissors'
         } else{
             return 'Lose. Paper beats rock'
         }
       }
         if (playerSelection.toLowerCase() === 'paper'){
-            if (computerSelection.toLowerCase() === 'paper'){
+            if (computerSelection === 3){
             return 'Draw. Paper does not beat paper'
-        } else if (computerSelection.toLowerCase() === 'rock'){
+        } else if (computerSelection === 1){
             return 'Win. Paper beats scissors'
         } else{
         return 'Lose. Scissors beats paper'
         }
       }
         if (playerSelection.toLowerCase() === 'scissors'){
-            if (computerSelection.toLowerCase() === 'scissors'){
+            if (computerSelection === 2){
                 return 'Draw. Scissors does not beat Scissors'
-        } else if (computerSelection.toLowerCase() === 'paper'){
+        } else if (computerSelection === 3){
             return 'Win! Scissors beats paper'
         } else{
             return 'Lose! Rock beats scossors'
         }
       }
     }
+
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
 return (playRound(playerSelection, computerSelection));
@@ -44,11 +42,11 @@ function game(){
     let compres = 0
     let playerres = 0
     let compturn = ''
-    let playertrun = ''
+    let playerturn = ''
     for (let i = 0; i < 5; i++){
         compturn = getComputerChoice()
-        playertrun = playerSelection
-        result = playRound(compturn, playertrun)
+        playerturn = playerSelection
+        result = playRound(compturn, playerturn)
         if (result.includes('Win')){
             playerres += 1
         } else if (result.includes('Lose')){
