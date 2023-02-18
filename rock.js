5315238fd64b0a3d8d5262c0bc03d467b28bdcfe
Ilia-hub61 <ilag61477@gmail.com>
2023-02-18 23:20:20 +0300

    noend

--- rock.js
+++ rock.js
@@ -1,66 +1,71 @@
-function getComputerChoice(number){
-    return Math.floor(Math.random() * number);
+function getComputerChoice() {
+  let number = Math.floor(Math.random() * 100);
+  if (number < 34) {
+    return "Rock";
+  } else if (number < 67) {
+    return "Scissors";
+  } else {
+    return "Paper";
+  }
 }
 
 function playRound(playerSelection, computerSelection) {
-    let playerSelection = prompt("Who's your fighter");
-        if (playerSelection.toLowerCase() === 'rock'){
-            if (computerSelection === 1){
-                return 'Draw. Rock does not beat rock'
-        } else if (computerSelection === 2){
-            return 'Win. Rock beats scissors'
-        } else{
-            return 'Lose. Paper beats rock'
-        }
-      }
-        if (playerSelection.toLowerCase() === 'paper'){
-            if (computerSelection === 3){
-            return 'Draw. Paper does not beat paper'
-        } else if (computerSelection === 1){
-            return 'Win. Paper beats scissors'
-        } else{
-        return 'Lose. Scissors beats paper'
-        }
-      }
-        if (playerSelection.toLowerCase() === 'scissors'){
-            if (computerSelection === 2){
-                return 'Draw. Scissors does not beat Scissors'
-        } else if (computerSelection === 3){
-            return 'Win! Scissors beats paper'
-        } else{
-            return 'Lose! Rock beats scossors'
-        }
-      }
+  if (playerSelection.toLowerCase() === "rock") {
+    if (computerSelection === "rock") {
+      return "Draw. Rock does not beat rock";
+    } else if (computerSelection === "scissors") {
+      return "Win. Rock beats scissors";
+    } else {
+      return "Lose. Paper beats rock";
     }
+  }
+  if (playerSelection.toLowerCase() === "paper") {
+    if (computerSelection === "paper") {
+      return "Draw. Paper does not beat paper";
+    } else if (computerSelection === "rock") {
+      return "Win. Paper beats rock";
+    } else {
+      return "Lose. Scissors beats paper";
+    }
+  }
+  if (playerSelection.toLowerCase() === "scissors") {
+    if (computerSelection === "scissors") {
+      return "Draw. Scissors does not beat Scissors";
+    } else if (computerSelection === "paper") {
+      return "Win! Scissors beats paper";
+    } else {
+      return "Lose! Rock beats scossors";
+    }
+  }
+}
 
 const playerSelection = "rock";
 const computerSelection = getComputerChoice();
-return (playRound(playerSelection, computerSelection));
 
-function game(){
-    let result = ''
-    let compres = 0
-    let playerres = 0
-    let compturn = ''
-    let playerturn = ''
-    for (let i = 0; i < 5; i++){
-        compturn = getComputerChoice()
-        playerturn = playerSelection
-        result = playRound(compturn, playerturn)
-        if (result.includes('Win')){
-            playerres += 1
-        } else if (result.includes('Lose')){
-            compres += 1
-        } else {
-            i -= 1
-        }
+function game() {
+  let result = "";
+  let compRes = 0;
+  let playerRes = 0;
+  let compTurn = "";
+  let playerTurn = "";
+  for (let i = 0; i < 5; i++) {
+    compTurn = getComputerChoice();
+    playerTurn = playerSelection;
+    result = playRound(compTurn, playerTurn);
+    if (result.includes("Win")) {
+      playerRes += 1;
+    } else if (result.includes("Lose")) {
+      compRes += 1;
+    } else {
+      i -= 1;
     }
+    console.log(result + " score " + playerRes + " : " + compRes);
+  }
+  if (playerRes > compRes) {
+    console.log("You win", playerRes + ":" + compRes);
+  } else {
+    console.log("You lose", compRes, +":" + playerRes);
+  }
 }
 
-if (playerres > compres){
-    console.log('You win', playerres + ':', + compres)
-} else{
-    console.log('You lose', compres, + ':', + playerres)
-}
-
-game()
+game();
